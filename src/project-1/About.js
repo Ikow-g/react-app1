ecf27dd8ccbb16e94946ca3f35c0b8b9f5bec79e
Muh. Rizal Sakti Djatmieka <muh.djatmieka@gmail.com>
2022-12-12 12:23:13 +0700

    update about

---
+++ src/project-1/About.js
@@ -0,0 +1,12 @@
+// import "./style.css";
+
+function About(){
+  return(
+    <div className="about">
+      <h3 className="subtitle">About</h3>
+      <p>Saya adalah seorang junior developer yang sedang memperdalam pengetahuan dalam website development.</p>
+    </div>
+  );
+}
+
+export default About;
