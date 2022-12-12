66b624a7dfb343ba0f6ae90466573813b4cd32c9
Muh. Rizal Sakti Djatmieka <muh.djatmieka@gmail.com>
2022-12-12 12:24:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Navbar from "./components/Navbar"
-import MainContent from "./components/MainContent"
+import "./project-1/style.css";
+// import Navbar from "./components/Navbar"
+// import MainContent from "./components/MainContent"
+import Info from "./project-1/Info";
+import About from "./project-1/About";
+import Interest from "./project-1/Interest";
+import Footer from "./project-1/Footer";
+
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <MainContent></MainContent>
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
+      {/* <Navbar></Navbar>
+      <MainContent></MainContent> */}
+      
     </div>
   );
 }
