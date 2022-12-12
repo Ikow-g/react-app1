672ec4b498ee1db23fb15343e8bcaa5ee517eb78
Muh. Rizal Sakti Djatmieka <muh.djatmieka@gmail.com>
2022-12-12 12:23:41 +0700

    new components

---
+++ src/project-1/Interest.js
@@ -0,0 +1,10 @@
+function Interest(){
+  return(
+    <div className="interest">
+      <h3 className="subtitle">Interests</h3>
+      <p>Saya sangat tertarik terhadap perkembangan teknologi. Saya merasa sangat senang saat berhasil mengaplikasikan suatu teknologi yang bermanfaat bagi banyak orang.</p>
+    </div>
+  );
+}
+
+export default Interest;
