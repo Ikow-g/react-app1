1f0ed07f65189ac580dbc7588cd6f9504e28083e
Muh. Rizal Sakti Djatmieka <muh.djatmieka@gmail.com>
2022-12-12 12:22:51 +0700

    add new project

---
+++ src/project-1/Info.js
@@ -0,0 +1,22 @@
+// import "./style.css";
+
+function Info(){
+  return(
+    <div className="info">
+      <img src="" alt="Profile Picture"></img>
+      <h3>Muh. Rizal Sakti Djatmieka</h3>
+      <h4>Front-end Developer</h4>
+      <h5>muh.djatmieka@binus.ac.id</h5>
+      <div className="contact">
+        <div className="email">
+          <button><img src="#"></img>Email</button>
+        </div>
+        <div className="linkedin">
+          <button><img src="#"></img>LinkedIn</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
