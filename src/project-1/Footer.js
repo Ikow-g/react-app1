3a9fb8d7a955731039ab01b96811d86dbe4aef29
Muh. Rizal Sakti Djatmieka <muh.djatmieka@gmail.com>
2022-12-12 12:48:35 +0700

    update footer

--- src/project-1/Footer.js
+++ src/project-1/Footer.js
@@ -1,7 +1,28 @@
 function Footer(){
   return (
     <footer className="footer">
-
+      <ul className="footer-contact">
+        <li>
+          <button href="#">
+            <img src="#" alt=""></img>
+          </button>
+        </li>
+        <li>
+          <button href="#">
+            <img src="#" alt=""></img>
+          </button>
+        </li>
+        <li>
+          <button href="#">
+            <img src="#" alt=""></img>
+          </button>
+        </li>
+        <li>
+          <button href="#">
+            <img src="#" alt=""></img>
+          </button>
+        </li>
+      </ul>
     </footer>
   );
 }
--- src/project-1/Info.js
+++ src/project-1/Info.js
@@ -3,16 +3,16 @@
 function Info(){
   return(
     <div className="info">
-      <img src="" alt="Profile Picture"></img>
+      <img src="#" alt=""></img>
       <h3>Muh. Rizal Sakti Djatmieka</h3>
       <h4>Front-end Developer</h4>
       <h5>muh.djatmieka@binus.ac.id</h5>
       <div className="contact">
         <div className="email">
-          <button><img src="#"></img>Email</button>
+          <button><img src="#" alt=""></img>Email</button>
         </div>
         <div className="linkedin">
-          <button><img src="#"></img>LinkedIn</button>
+          <button><img src="#" alt=""></img>LinkedIn</button>
         </div>
       </div>
     </div>
